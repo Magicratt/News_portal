a4224e13db04c602a7fc1449fda01ac7a4110138
Егоркин <egorkinmihail000@gmail.com>
2023-05-23 18:30:12 +0300

    Initial commit

---
+++ flask_news/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+
+db = SQLAlchemy(app)
+
+from . import models
+
+with app.app_context():
+    db.create_all()
+
+from . import views
+
